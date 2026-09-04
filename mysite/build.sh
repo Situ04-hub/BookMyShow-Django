6c3938efbfd7f859121830d3bd2a0dace0573d3d
Aniket Das <aniketdas162005@gmail.com>
2026-09-04 12:37:38 +0530

    Bypass PEP 668 in Vercel build

--- mysite/build.sh
+++ mysite/build.sh
@@ -2,6 +2,6 @@
 # Exit on error
 set -o errexit
 
-python3 -m pip install -r ../requirements.txt
+python3 -m pip install -r ../requirements.txt --break-system-packages
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
